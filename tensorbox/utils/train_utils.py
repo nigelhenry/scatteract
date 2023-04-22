7b8d7f8b74bfeb7dc4b45e82a483f1e7738e9eca
Nigel Henry <nigelhenry@gmail.com>
2023-04-22 00:20:08 -0400

    Update train_utils.py

--- tensorbox/utils/train_utils.py
+++ tensorbox/utils/train_utils.py
@@ -15,8 +15,8 @@ import os
 current_dir = os.path.dirname(__file__)
 sys.path.append(current_dir+'/annolist')
 sys.path.append(current_dir+'/rect')
-from annolist import AnnotationLib as al
-from rect import Rect
+import AnnotationLib as al
+import Rect
 
 def rescale_boxes(current_shape, anno, target_height, target_width):
     x_scale = target_width / float(current_shape[1])
